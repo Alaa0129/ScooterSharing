d5a0e16546211478ca30994f4a7361d61937d582
Alaa Abdul-Al <alia@itu.dk>
2023-02-01 01:18:12 +0100

    almost done

--- MADSH/app/src/main/java/dk/itu/moapd/scootersharing/alia/MainActivity.kt
+++ MADSH/app/src/main/java/dk/itu/moapd/scootersharing/alia/MainActivity.kt
@@ -2,10 +2,36 @@ package dk.itu.moapd.scootersharing.alia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
+
+    // A set of private constants used in this class.
+    companion object {
+        private val TAG = MainActivity::class.qualifiedName
+    }
+
+    // GUI variables.
+    private lateinit var scooterName: EditText
+    private lateinit var locationName: EditText
+    private lateinit var startRideBtn: Button
+
+    private val scooter:Scooter = Scooter("", "")
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    // Edit texts.
+    
+
+    private fun showMessage () {
+        // Print a message in the ‘Logcat‘ system.
+        Log.d(TAG, scooter.toString())
     }
 }
