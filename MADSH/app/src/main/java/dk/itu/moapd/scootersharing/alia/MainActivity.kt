63dba39450bac31e090fa39d2400810b2c4c109b
Alaa Abdul-Al <58032582+Alaa0129@users.noreply.github.com>
2023-02-01 18:38:26 +0100

    Exercise 1 Done (#1)
    * almost done
    
    * Done with exercise 1

--- MADSH/app/src/main/java/dk/itu/moapd/scootersharing/alia/MainActivity.kt
+++ MADSH/app/src/main/java/dk/itu/moapd/scootersharing/alia/MainActivity.kt
@@ -2,10 +2,56 @@ package dk.itu.moapd.scootersharing.alia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
+
+    // A set of private constants used in this class.
+    companion object {
+        private val TAG = MainActivity::class.qualifiedName
+    }
+
+    // GUI variables.
+    private lateinit var scooterName: com.google.android.material.textfield.TextInputEditText
+    private lateinit var scooterLocation: com.google.android.material.textfield.TextInputEditText
+    private lateinit var startRideBtn: Button
+
+    private val scooter:Scooter = Scooter("", "")
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Edit texts.
+        scooterName = findViewById(R.id.edit_text_name)
+        scooterLocation = findViewById(R.id.edit_text_location)
+
+        // Buttons.
+        startRideBtn = findViewById(R.id.start_ride_button)
+
+        startRideBtn.setOnClickListener() {
+            if (scooterName.text!!.isNotEmpty() && scooterLocation.text!!.isNotEmpty()) {
+
+                // Update the object attributes.
+                val nameScooter = scooterName.text.toString().trim()
+                val nameLocation = scooterLocation.text.toString().trim()
+
+                scooter.setName(nameScooter)
+                scooter.setLocation(nameLocation)
+
+                // Reset the text fields and update the UI.
+                scooterName.text?.clear()
+                scooterLocation.text?.clear()
+                showMessage()
+            }
+        }
+    }
+
+    private fun showMessage () {
+        // Print a message in the ‘Logcat‘ system.
+        Log.d(TAG, scooter.toString())
     }
 }
