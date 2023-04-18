c0217904633693a7aaf0e023d3418ce3b75cc55d
Alaa Abdul-Al <alia@itu.dk>
2023-04-18 13:58:14 +0200

    added login

---
+++ MADSH/app/src/main/java/dk/itu/moapd/scootersharing/alia/LoginActivity.kt
@@ -0,0 +1,10 @@
+package dk.itu.moapd.scootersharing.alia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
