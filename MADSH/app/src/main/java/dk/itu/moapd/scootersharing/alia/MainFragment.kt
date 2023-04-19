8dfda0f71ddf8e7b36b782662788d55c875c4719
Alaa Abdul-Al <alia@itu.dk>
2023-04-19 13:45:01 +0200

    WIP. Ex. 1 done.

--- MADSH/app/src/main/java/dk/itu/moapd/scootersharing/alia/MainFragment.kt
+++ MADSH/app/src/main/java/dk/itu/moapd/scootersharing/alia/MainFragment.kt
@@ -1,5 +1,6 @@
 package dk.itu.moapd.scootersharing.alia
 
+import android.Manifest
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -10,6 +11,21 @@ import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import android.content.pm.PackageManager
+import android.content.res.Configuration
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat.checkSelfPermission
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.math.log
 import dk.itu.moapd.scootersharing.alia.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
@@ -20,11 +36,28 @@ class MainFragment : Fragment() {
         }
     private lateinit var auth: FirebaseAuth
 
+    companion object {
+        private val TAG = MainFragment::class.qualifiedName
+        private const val ALL_PERMISSIONS_RESULT = 1011
+    }
+
+    /**
+     * The primary instance for receiving location updates.
+     */
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    /**
+     * This callback is called when `FusedLocationProviderClient` has a new `Location`.
+     */
+    private lateinit var locationCallback: LocationCallback
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Initialize Firebase Auth.
         auth = Firebase.auth
+
+        requestUserPermissions()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
@@ -68,4 +101,42 @@ class MainFragment : Fragment() {
             startLoginActivity()
         }
     }
+
+    /**
+     * Create a set of dialogs to show to the users and ask them for permissions to get the device's
+     * resources.
+     */
+    private fun requestUserPermissions() {
+
+        // An array with location-aware permissions.
+        val permissions: ArrayList<String> = ArrayList()
+        permissions.add(Manifest.permission.ACCESS_FINE_LOCATION)
+        permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION)
+
+        // Check which permissions is needed to ask to the user.
+        val permissionsToRequest = permissionsToRequest(permissions)
+
+        // Show the permissions dialogs to the user.
+        if (permissionsToRequest.size > 0)
+            activity?.requestPermissions(
+                permissionsToRequest.toTypedArray(),
+                ALL_PERMISSIONS_RESULT
+            )
+    }
+
+    /**
+     * Create an array with the permissions to show to the user.
+     *
+     * @param permissions An array with the permissions needed by this applications.
+     *
+     * @return An array with the permissions needed to ask to the user.
+     */
+    private fun permissionsToRequest(permissions: ArrayList<String>): ArrayList<String> {
+        val result: ArrayList<String> = ArrayList()
+        for (permission in permissions)
+            if (checkSelfPermission(requireContext(), permission) != PackageManager.PERMISSION_GRANTED)
+                result.add(permission)
+        return result
+    }
+
 }
