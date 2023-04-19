aa392410371ac0860a1dc715852e083f22052dac
Alaa Abdul-Al <alia@itu.dk>
2023-04-19 13:20:15 +0200

    WIP. Ex. 1 done. Almost done with ex.2

--- MADSH/app/src/main/java/dk/itu/moapd/scootersharing/alia/MainFragment.kt
+++ MADSH/app/src/main/java/dk/itu/moapd/scootersharing/alia/MainFragment.kt
@@ -1,16 +1,33 @@
 package dk.itu.moapd.scootersharing.alia
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.res.Configuration
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat.checkSelfPermission
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import dk.itu.moapd.scootersharing.alia.databinding.FragmentMainBinding
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.math.log
 
 class MainFragment : Fragment() {
     private var _binding: FragmentMainBinding? = null
@@ -20,11 +37,29 @@ class MainFragment : Fragment() {
         }
     private lateinit var auth: FirebaseAuth
 
+    companion object {
+        private val TAG = MainFragment::class.qualifiedName
+        private const val ALL_PERMISSIONS_RESULT = 1011
+    }
+
+    /**
+     * The primary instance for receiving location updates.
+     */
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    /**
+     * This callback is called when `FusedLocationProviderClient` has a new `Location`.
+     */
+    private lateinit var locationCallback: LocationCallback
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Initialize Firebase Auth.
         auth = Firebase.auth
+
+        // Start the location-aware method.
+        startLocationAware()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
@@ -67,5 +102,150 @@ class MainFragment : Fragment() {
             auth.signOut()
             startLoginActivity()
         }
+    }
+
+    private fun startLocationAware() {
+
+        // Show a dialog to ask the user to allow the application to access the device's location.
+        requestUserPermissions()
+
+        // Start receiving location updates.
+        fusedLocationProviderClient = LocationServices
+            .getFusedLocationProviderClient(requireContext())
+
+        // Initialize the `LocationCallback`.
+        locationCallback = object : LocationCallback() {
+
+            /**
+             * This method will be executed when `FusedLocationProviderClient` has a new location.
+             *
+             * @param locationResult The last known location.
+             */
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+
+                // Updates the user interface components with GPS data location.
+                locationResult.lastLocation?.let { location ->
+                    updateUI(location)
+                }
+            }
+        }
+    }
+
+    /**
+     * Create a set of dialogs to show to the users and ask them for permissions to get the device's
+     * resources.
+     */
+    private fun requestUserPermissions() {
+
+        // An array with location-aware permissions.
+        val permissions: ArrayList<String> = ArrayList()
+        permissions.add(Manifest.permission.ACCESS_FINE_LOCATION)
+        permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION)
+
+        // Check which permissions is needed to ask to the user.
+        val permissionsToRequest = permissionsToRequest(permissions)
+
+        // Show the permissions dialogs to the user.
+        if (permissionsToRequest.size > 0)
+            activity?.requestPermissions(
+                permissionsToRequest.toTypedArray(),
+                ALL_PERMISSIONS_RESULT
+            )
+    }
+
+    /**
+     * Create an array with the permissions to show to the user.
+     *
+     * @param permissions An array with the permissions needed by this applications.
+     *
+     * @return An array with the permissions needed to ask to the user.
+     */
+    private fun permissionsToRequest(permissions: ArrayList<String>): ArrayList<String> {
+        val result: ArrayList<String> = ArrayList()
+        for (permission in permissions)
+            if (checkSelfPermission(requireContext(), permission) != PackageManager.PERMISSION_GRANTED)
+                result.add(permission)
+        return result
+    }
+
+    /**
+     * This method checks if the user allows the application uses all location-aware resources to
+     * monitor the user's location.
+     *
+     * @return A boolean value with the user permission agreement.
+     */
+    private fun checkPermission() =
+        ActivityCompat.checkSelfPermission(
+            requireContext(), Manifest.permission.ACCESS_FINE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(
+                    requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+
+    /**
+     * Update the UI components based on the current device's location data.
+     *
+     * @param location The current location data.
+     */
+    private fun updateUI(location: Location) {
+        if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT)
+            Log.d(TAG,"Latitude: ${location.latitude}, Longitude: ${location.longitude}")
+        else
+            setAddress(location.latitude, location.longitude)
+    }
+
+    /**
+     * Use Geocoder API to convert the current location into a `String` address, and update the
+     * corresponding UI component.
+     *
+     * @param latitude The current latitude coordinate.
+     * @param longitude The current longitude coordinate.
+     */
+    private fun setAddress(latitude: Double, longitude: Double) {
+        if (!Geocoder.isPresent())
+            return
+
+        // Create the `Geocoder` instance.
+        val geocoder = Geocoder(requireContext(), Locale.getDefault())
+
+        // After `Tiramisu Android OS`, it is needed to use a listener to avoid blocking the main
+        // thread waiting for results.
+        val geocodeListener = Geocoder.GeocodeListener { addresses ->
+            addresses.firstOrNull()?.toAddressString()?.let { address ->
+                Log.d(TAG, "Address: $address")
+            }
+        }
+
+        // Return an array of Addresses that attempt to describe the area immediately surrounding
+        // the given latitude and longitude.
+        if (Build.VERSION.SDK_INT >= 33)
+            geocoder.getFromLocation(latitude, longitude, 1, geocodeListener)
+        else
+            geocoder.getFromLocation(latitude, longitude, 1)?.let {  addresses ->
+                addresses.firstOrNull()?.toAddressString()?.let { address ->
+                    Log.d(TAG, "Address: $address")
+                }
+            }
+    }
+
+    /**
+     * Converts the `Address` instance into a `String` representation.
+     *
+     * @return A `String` with the current address.
+     */
+    private fun Address.toAddressString() : String {
+        val address = this
+
+        // Create a `String` with multiple lines.
+        val stringBuilder = StringBuilder()
+        stringBuilder.apply {
+            append(address.getAddressLine(0)).append("\n")
+            append(address.postalCode).append(" ")
+            append(address.locality).append("\n")
+            append(address.countryName)
+        }
+
+        return stringBuilder.toString()
     }
 }
