0576801860d54ce1e664d4cff6d19c6d2a3bbc7c
Joachim Alexander Kofoed <jak-ak-k@hotmail.com>
2023-05-05 13:10:38 +0200

    WIP

--- MADSH/app/src/main/java/dk/itu/moapd/scootersharing/alia/models/Scooter.kt
+++ MADSH/app/src/main/java/dk/itu/moapd/scootersharing/alia/models/Scooter.kt
@@ -5,19 +5,4 @@ data class Scooter (
     var latitude: Double = 0.0,
     var longitude: Double = 0.0,
     var available: Boolean = true,
-    var lastPhoto: String = "") {
-
-    override fun toString():String {
-        return "'$name' is placed at ${coordinatesToLocation()}"
-    }
-
-    private fun coordinatesToLocation(): String {
-        return ""
-        //return "($latitude, $longitude)"
-    }
-
-    private fun dateFormatted(): String {
-        return ""
-        //return DateFormat.getDateTimeInstance().format(timestamp)
-    }
-}
+    var lastPhoto: String = "")
